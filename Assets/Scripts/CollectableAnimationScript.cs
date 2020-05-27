054f2251752e9c2a381c3649bc2c0146af3cbaa0
Andreea <aro1u17@soton.ac.uk>
2020-05-27 18:12:51 +0300

    Layer 2 - 9 Idle and Destroy Animations In

---
+++ Assets/Scripts/CollectableAnimationScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableAnimationScript : MonoBehaviour
+{
+    private Animator animator;
+
+    private Camera cam;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+    
+    void Update()
+    {
+        if (OnScreen())
+        {
+            animator.SetBool("playIdle", true);
+        }
+        else
+        {
+            animator.SetBool("playIdle", false);
+        }
+    }
+
+    private bool OnScreen()
+    {
+        Vector3 screenPoint = cam.WorldToViewportPoint(transform.position);
+
+        if (screenPoint.x > -0.2f && screenPoint.x < 1.2f && screenPoint.y > -0.2f && screenPoint.y < 1.2f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
